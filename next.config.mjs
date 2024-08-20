a4720e93f0d019ed819da581cdf211ff2be830ff
kyboahene <kyeiyaw437@gmail>
2024-08-20 14:01:22 +0000

    config images added
    Signed-off-by: kyboahene <kyeiyaw437@gmail>

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+    domains: [
+      'via.placeholder.com',
+      'firebasestorage.googleapis.com',
+      'lh3.googleusercontent.com',
+      'res.cloudinary.com',
+    ],
+  },
+};
 
 export default nextConfig;

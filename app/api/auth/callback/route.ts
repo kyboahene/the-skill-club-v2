09de87e16f96bb3eca8c387bdb4b34c0fdca285b
kyboahene <kyeiyaw437@gmail>
2024-09-08 08:37:27 +0000

    feat(api): add an endpoint for auth callbacks
    Signed-off-by: kyboahene <kyeiyaw437@gmail>

---
+++ app/api/auth/callback/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+
+import { createClient } from "@/supabase/server";
+
+export async function GET(request: Request) {
+
+    const requestUrl = new URL(request.url);
+    const code = requestUrl.searchParams.get("code");
+
+    if (code) {
+        const supabase = createClient();
+        await supabase.auth.exchangeCodeForSession(code);
+    }
+
+    return NextResponse.redirect(requestUrl.origin);
+}

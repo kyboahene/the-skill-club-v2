e957378b46c0d9303735ad2e9a8211f6ca649453
kyboahene <kyeiyaw437@gmail>
2024-09-08 08:43:43 +0000

    refactor
    Signed-off-by: kyboahene <kyeiyaw437@gmail>

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -8,16 +8,6 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 
-export const Capitalize = (word: string) => {
-  const words = word.split(' ');
-  const capitalizedWords = words.map(word => {
-    const firstLetter = word.charAt(0).toUpperCase();
-    const restOfWord = word.slice(1);
-    return firstLetter + restOfWord;
-  });
-  return capitalizedWords.join(' ');
-};
-
 // export function showErrorMessage(title: string, error: any) {
 //   return toast.error(title, {
 //     position: "top-right",
@@ -40,21 +30,4 @@ export function showSuccessMessage(title: string, message: string) {
     position: "top-right",
     description: message,
   });
-}
-
-
-export const requestOptions = {
-  method: "GET",
-  headers: { "Content-Type": "application/json" },
-};
-
-export function defaultOptions(json: any) {
-  return {
-    loop: true,
-    autoplay: true,
-    animationData: json,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
 }

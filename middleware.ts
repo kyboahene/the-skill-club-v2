6720c7206a25bf50fe94467d5760a1b85edd5138
kyboahene <kyeiyaw437@gmail>
2024-09-08 08:14:41 +0000

    feat: added redirects for all users
    Signed-off-by: kyboahene <kyeiyaw437@gmail>

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,20 @@
-import { type NextRequest } from "next/server"
+import { NextResponse, type NextRequest } from "next/server"
 import { updateSession } from "@/supabase/middleware"
 
+const protectedRoutes: string[] = ['/dashboard']
 
 export async function middleware(request: NextRequest) {
-    return await updateSession(request)
+    let { user, response } = await updateSession(request)
+
+    if (!user && protectedRoutes.some(pr => request.nextUrl.pathname.startsWith(pr))) {
+        response = NextResponse.redirect(new URL('/auth/login', request.url))
+    } else {
+        response = NextResponse.next()
+    }
+
+    return response
 }
 
 export const config = {
-    matcher: []
+    matcher: ['/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)']
 }

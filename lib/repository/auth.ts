7bd03ea87b2d6b5414a08f0b62755b2018ca6442
kyboahene <kyeiyaw437@gmail>
2024-09-08 08:34:47 +0000

    feat(api): add a repository for authentication
    Signed-off-by: kyboahene <kyeiyaw437@gmail>

--- lib/repository/auth.ts
+++ lib/repository/auth.ts
@@ -1,25 +1,37 @@
+import { env } from "@/env"
 import { createClient } from "@/supabase/server"
-import { Provider } from "@supabase/supabase-js"
+import { Provider, SupabaseClient } from "@supabase/supabase-js"
 
 export class AuthRepository {
-    supabase = createClient()
+    private supabase: SupabaseClient
 
-    constructor() { }
-
-    signUpWithEmail(email: string, password: string) {
-        return this.supabase.auth.signUp({ email, password })
+    constructor() {
+        this.supabase = createClient()
     }
 
-    loginWithEmail(email: string, password: string) {
+    signUp(email: string, password: string) {
+        return this.supabase.auth.signUp({
+            email, password, options: {
+                emailRedirectTo: `${env.BASE_URL}/auth/callback`
+            }
+        })
+    }
+
+    loginWithPassword(email: string, password: string) {
         return this.supabase.auth.signInWithPassword({ email, password })
     }
 
-    oAuthSignIn(provider: Provider, redirectUrl: string) {
-        return this.supabase.auth.signInWithOAuth({
-            provider,
-            options: {
-                redirectTo: redirectUrl,
-            }
+    oAuthSignIn(provider: Provider) {
+        return this.supabase.auth.signInWithOAuth({ provider })
+    }
+
+    forgotPassword(email: string, redirectTo: string) {
+        return this.supabase.auth.resetPasswordForEmail(email, { redirectTo })
+    }
+
+    resetPassword(password: string) {
+        return this.supabase.auth.updateUser({
+            password
         })
     }
 }

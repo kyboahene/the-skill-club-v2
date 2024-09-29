9331ed83f9642a3f04b7bbf7d1ff76b4546c1f0e
kyboahene <kyeiyaw437@gmail>
2024-09-29 10:16:10 +0000

    refactor: replaced domains with remote patterns
    Signed-off-by: kyboahene <kyeiyaw437@gmail>

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-    domains: [
-      'via.placeholder.com',
-      'firebasestorage.googleapis.com',
-      'lh3.googleusercontent.com',
-      'res.cloudinary.com',
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "via.placeholder.com",
+        port: "",
+        pathname: "**"
+      },
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+        port: "",
+        pathname: "**"
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "**"
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "**"
+      }
     ],
   },
 };

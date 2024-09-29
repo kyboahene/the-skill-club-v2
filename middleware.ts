51a74707e2d8869cfc5aa42a26adfad3e89afc2a
kyboahene <kyeiyaw437@gmail>
2024-09-29 10:15:21 +0000

    feat: made modification to handle unauthenticated users
    Signed-off-by: kyboahene <kyeiyaw437@gmail>

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,28 @@
 import { NextResponse, type NextRequest } from "next/server"
 import { updateSession } from "@/supabase/middleware"
 
-const protectedRoutes: string[] = ['/dashboard']
+const protectedRoutes: string[] = ['/dashboard', '/company', '/admin'];
 
 export async function middleware(request: NextRequest) {
-    let { user, response } = await updateSession(request)
+    let { user, response } = await updateSession(request);
 
     if (!user && protectedRoutes.some(pr => request.nextUrl.pathname.startsWith(pr))) {
-        response = NextResponse.redirect(new URL('/auth/login', request.url))
+        let redirectUrl = '/auth/login';
+        if (request.nextUrl.pathname.startsWith('/dashboard')) {
+            redirectUrl = '/auth/login?redirect=/dashboard';
+        } else if (request.nextUrl.pathname.startsWith('/company')) {
+            redirectUrl = '/auth/login?redirect=/company';
+        } else if (request.nextUrl.pathname.startsWith('/admin')) {
+            redirectUrl = '/auth/login?redirect=/admin';
+        }
+        response = NextResponse.redirect(new URL(redirectUrl, request.url));
     } else {
-        response = NextResponse.next()
+        response = NextResponse.next();
     }
 
-    return response
+    return response;
 }
 
 export const config = {
-    matcher: ['/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)']
-}
+    matcher: ['/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)'],
+};
